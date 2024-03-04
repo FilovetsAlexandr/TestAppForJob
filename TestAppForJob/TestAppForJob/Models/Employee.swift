0ee5be73650b1135a5d1dcf19ac378b81c7c0661
FilovetsAlexandr <sohan.boxan@gmail.com>
2024-03-04 22:04:33 +0300

    MVVM + delete storyboard

---
+++ TestAppForJob/TestAppForJob/Models/Employee.swift
@@ -0,0 +1,19 @@
+//
+//  Employee.swift
+//  TestAppForJob
+//
+//  Created by Alexandr Filovets on 4.03.24.
+//
+
+import Foundation
+
+struct Employee {
+    let id: Int
+    let firstName: String
+    let lastName: String
+    let nickname: String
+    let department: String
+    let birthDate: Date
+    let phoneNumber: String
+    let photoURL: URL
+}
