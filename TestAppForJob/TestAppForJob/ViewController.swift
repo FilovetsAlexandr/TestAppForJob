3783f1f59691660c7d3e04a338918eb808fb96de
FilovetsAlexandr <sohan.boxan@gmail.com>
2024-03-04 21:43:30 +0300

    Project created

---
+++ TestAppForJob/TestAppForJob/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestAppForJob
+//
+//  Created by Alexandr Filovets on 4.03.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
